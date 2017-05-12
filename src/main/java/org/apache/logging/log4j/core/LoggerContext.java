d0a1caa1907867414dcf4d9b46e55eade566fed7
Jens Reimann <jreimann@redhat.com>
2017-05-12 13:26:44 +0200

    Initial import

---
+++ src/main/java/org/apache/logging/log4j/core/LoggerContext.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2017 Jens Reimann <jreimann@redhat.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.logging.log4j.core;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+
+public class LoggerContext {
+
+    public static LoggerContext getContext() {
+        return new LoggerContext();
+    }
+
+    public static LoggerContext getContext(final boolean currentContext) {
+        return getContext();
+    }
+
+    public static LoggerContext getContext(final ClassLoader loader, final boolean currentContext, final URI configLocation) {
+        return getContext();
+    }
+
+    public Configuration getConfiguration() {
+        return new Configuration() {
+
+            public Map<String, LoggerConfig> getLoggers() {
+                return Collections.emptyMap();
+            }
+        };
+    }
+}
